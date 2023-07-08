1ec188e4b3dc3cb5ba9520a9dc2701ff84043256
blinkEgor <barminov44@gmail.com>
2023-07-08 20:43:17 +0300

    corrected

--- bit-conversion/index.js
+++ bit-conversion/index.js
@@ -2,25 +2,26 @@ const convert = document.getElementById('convert');
 const decimalInput = document.getElementById('decimal')
 const resultP = document.getElementById('result')
 
+const check = (original, count, val, res) => {
+    if(original % 2) {
+        let print = `${original} = ${count}`
+        val.value = ''
+        console.log(print)
+        return res.innerText = print
+    } else {
+        let print = `${original} = ${count.split('').reverse().join('')}`
+        val.value = ''
+        console.log(print)
+        return res.innerText = print
+    }
+}
+
 const toBit = (val, res) => {
         let original = parseFloat(val.value); console.log(original)
         let decimal = parseFloat(val.value)
         let count = '';
         if(decimal > 0) {
             for(let i = 0; i < val.value; i++) {
-                if (decimal < 1) {
-                    if(original % 2) {
-                        let print = `${original} = ${count}`
-                        val.value = ''
-                        console.log(print)
-                        return res.innerText = print
-                    } else {
-                        let print = `${original} = ${count.split('').reverse().join('')}`
-                        val.value = ''
-                        console.log(print)
-                        return res.innerText = print
-                    }
-                }
                 if (decimal % 2) {
                     count += '1'
                     decimal = (Math.round(decimal) - 1)
@@ -34,17 +35,15 @@ const toBit = (val, res) => {
                 }
                 decimal /= 2;
                 console.log(decimal); console.log(original)
+                if (decimal < 1) {
+                    check(original, count, val, res)
+                }
             }
-        }
-        if(original % 2) {
-            let print = `${original} = ${count}`
-            console.log(print)
-            return res.innerText = print
         } else {
-            let print = `${original} = ${count.split('').reverse().join('')}`
-            console.log(print)
-            return res.innerText = print
+            count = '0'
+            check(original, count, val, res)    
         }
+        check(original, count, val, res)
 }
 
 convert.addEventListener("click", () => toBit(decimalInput, resultP));
