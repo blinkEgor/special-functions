3f63f301a985a558796b734facb5fe00f7b80836
blinkEgor <barminov44@gmail.com>
2023-10-25 14:59:43 +0300

    add Entity class

--- gameTemplate/index.js
+++ gameTemplate/index.js
@@ -143,3 +143,128 @@ class Inventory {
         return this.container[idx];
     }
 }
+
+// ________________________________________________________________
+
+class Entity {
+    max_health;
+    health;
+    shield = 0;
+    inventory = new Inventory(16);
+    effects = Array();
+    steps;
+    max_steps;
+    weapon_idx = -1;
+
+    constructor(max_health = 100, max_steps = 3) {
+        this.max_health = max_health;
+        this.health = max_health;
+        this.steps = max_steps;
+        this.max_steps = max_steps;
+    }
+
+    gain_damage(damage, effect) {
+        if(effect !== null) {
+            this.effects.push(effect);
+        }
+        let dmg = damage;
+        for(const effect in this.effects) {
+            if(effect.get_resistance !== null) {
+                dmg *= (1 - effect.get_resistance());
+            }
+        }
+        let dmg_left = dmg - this.shield;
+        if(dmg_left >= 0) {
+            this.health = this.health - dmg_left;
+            this.reset_shield();
+        } else {
+            this.shield -= damage;
+        }
+        if(this.health < 0) {
+            this.health = 0;
+        }
+    }
+
+    setup_shield(shield_count, effect) {
+        if(effect !== null) {
+            this.effects.push(effect);
+        }
+        let def = shield_count;
+        this.effects.forEach(effect => {
+            if(effect.get_def_incrementation !== null) {
+                def += effect.get_def_incrementation();
+            }
+        });
+        this.effects.forEach(effect => {
+            if(effect.get_def_multiplier !== null) {
+                def *= effect.get_def_multiplier();
+            }
+        });
+        this.shield += def;
+    }
+
+    reset_shield() {
+        this.shield = 0;
+    }
+
+    heal(heal_count, effect = null) {
+        if(effect !== null) {
+            this.effects.push(effect);
+        }
+        if(this.health < this.max_health) {
+            this.health += heal_count;
+        }
+        if(this.health > this.max_health) {
+            this.health = this.max_health;
+        }
+    }
+
+    deal_damage(target, weapon) {
+        if(weapon instanceof Weapon) {
+            let dmg = weapon.get_damage();
+            this.effects.forEach(effect => {
+                if(effect.get_dmg_incrementation !== null) {
+                    dmg += effect.get_dmg_incrementation();
+                }
+            });
+            this.effects.forEach(effect => {
+                if(effect.get_dmg_multiplier !== null) {
+                    dmg *= effect.get_dmg_multiplier();
+                }
+            });
+            target.gain_damage(dmg, weapon.get_effect());
+        }
+    }
+
+    is_alive() {
+        return this.health > 0;
+    }
+
+    step() {
+        for(let i = 0; i < this.effects.length; i++) {
+            const effect = this.effects[i];
+            if(effect.get_damage !== null) {
+                this.gain_damage(effect.get_damage(), null);
+            }
+            if(effect.get_heal_count !== null) {
+                this.heal(effect.get_heal_count !== null);
+            }
+            if(effect.get_armor !== null) {
+                this.heal(effect.get_armor(), null);
+            }
+            effect.count();
+            if(effect.get_steps_left() <= 0) {
+                this.effects[i] = null;
+            }
+        }
+        this.effects = this.effects.filter(elem => elem != null);
+    }
+
+    get_weapon() {
+        if(this.weapon_idx >= 0) {
+            return this.inventory.get_item(this.weapon_idx);
+        } else {
+            return null;
+        }
+    }
+}
